985903815c7ff043b1d898663bb83eeb203edc7c
Matthijs van der Meulen <matthijsvdmeulen@gmail.com>
2021-11-18 13:40:40 +0100

    Add preload info

--- main.js
+++ main.js
@@ -1,19 +1,23 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
 function createWindow () {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600
-  })
+    width: 600,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  });
 
-  win.loadFile('index.html')
+  win.loadFile('index.html');
 }
 
 app.whenReady().then(() => {
-  createWindow()
+  createWindow();
 
-})
+});
 
 app.on('window-all-closed', function () {
-  app.quit()
-})
+  app.quit();
+});
